28e3b6563458a2e3228ec1fdd377b3fad57a4b87
Mark Piper <mark.piper@colorado.edu>
2019-09-10 16:51:20 -0600

    Add build script
    Set coretran_DIR instead of CMAKE_PREFIX_PATH.

---
+++ recipe/build.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+if [[ `uname -s` == 'Darwin' ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=""
+fi
+
+mkdir _build && cd _build
+NETCDF_DIR=$PREFIX cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -Dcoretran_DIR=$PREFIX/lib/cmake \
+    -DBUILD_SHARED_LIBS=ON \
+    ../src
+make -j$CPU_COUNT
+make install
